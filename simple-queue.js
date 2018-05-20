b77b59382214da23d74944b70e0bea326c757c86
Tobias Gassmann <tobias@gaszmann.de>
2018-05-20 19:44:36 +0200

    Create simple-queue.js

---
+++ simple-queue.js
@@ -0,0 +1,51 @@
+var SimpleTaskQueue = function(){
+  var self = this;
+  var queueHasStopped = true;
+  var queueShouldRun = false;
+
+  var tasks = [];
+
+  self.addTask = function(task){
+    tasks.push(task);
+    if (queueHasStopped && queueShouldRun){
+      self.run();
+    }
+  }
+
+  self.run = function(){
+    queueShouldRun = true;
+    queueHasStopped = false;
+    self.runNextTask();
+  };
+
+  self.stop = function(){
+    queueShouldRun = false;
+  };
+
+
+  self.runNextTask = function(){
+
+    var task = tasks.shift();
+    if (task === undefined){
+      queueHasStopped = true;
+    }
+    else {
+      console.log('executing one task');
+      task(function(){
+        setTimeout(function(){
+          console.log('in the callback, task has finished');
+          if (queueShouldRun){
+            self.runNextTask();
+          }
+          else {
+            queueHasStopped = true;
+          }
+        },0);              
+      });
+    }
+
+  };
+
+};
+
+
